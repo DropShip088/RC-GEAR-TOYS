2b2ca2628feb3e9553b477825d96450dbdca4eef
DropShip088 <dropbisiness01@gmail.com>
2026-06-20 13:37:07 +0530

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "node:path";
 
 export default defineConfig({
+  base: "/RC-GEAR-TOYS/",
   build: {
     rollupOptions: {
       input: {
